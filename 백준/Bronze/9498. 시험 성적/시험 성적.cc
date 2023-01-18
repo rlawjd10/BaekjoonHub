9b0f141f1272dd03feb49e90355374dbaee8b58d
jeongeun <rlawjd10eun@gmail.com>
2023-01-18 12:31:29 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int grade;
+    cin >> grade;
+    
+    
+    if (grade>=90)
+        cout << "A" << endl;
+    else if (grade >= 80)
+        cout << "B" << endl;
+    else if (grade >= 70)
+        cout << "C" << endl;
+    else if (grade >= 60)
+        cout << "D" << endl;
+    else 
+        cout << "F" << endl;
+    
+    return 0;
+}
